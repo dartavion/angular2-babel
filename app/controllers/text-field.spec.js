818425d17b92f91d2875010b137c7f30dcaa7df9
Richard Ranke <richard.ranke@salesfusion.com>
2015-12-28 15:09:28 -0500

    Adding basic test and updating to systemjs, karma, jasmine and babel

---
+++ app/controllers/text-field.spec.js
@@ -0,0 +1,8 @@
+"use strict";
+import TextField from './text-field.js';
+describe('Ha', function () {
+  it('should equal', function () {
+    expect(1).toEqual(1);
+  });
+
+});
---
+++ system.conf.js
@@ -0,0 +1,9 @@
+System.config({
+  paths: {
+    'babel': 'node_modules/babel-core/browser.js',
+    'systemjs': 'node_modules/systemjs/dist/system.js',
+    'system-polyfills': 'node_modules/systemjs/dist/system-polyfills.js',
+    'es6-module-loader': 'node_modules/es6-module-loader/dist/es6-module-loader.js'
+  },
+  transpiler: 'babel'
+});
